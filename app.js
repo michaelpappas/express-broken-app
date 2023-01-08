b497df1b0d7d21b4a8ebfb5bc0fd81dbc2e4d5d0
michaelpappas <pappas.michael@gmail.com>
2023-01-08 15:42:45 -0800

    moved axios map to separate function

--- app.js
+++ app.js
@@ -1,19 +1,39 @@
 const express = require('express');
-let axios = require('axios');
-var app = express();
+const app = express();
+const axios = require('axios');
+const { NotFoundError } = require("./expressError");
+
 
 // Parse request bodies for JSON
 app.use(express.json());
 
+/** takes in the usernames in the developers array from the request
+   * returns json with the name and bio of each developer
+   */
 app.post('/', async function (req, res, next) {
-  let results = await Promise.all(req.body.developers.map(async d =>
-              axios.get(`https://api.github.com/users/${d}`)));
 
-  let out = results.map(r => ({ "name": r.data.name, "bio": r.data.bio }));
+  const developers = req.body.developers
 
-  return res.send(JSON.stringify(out));
+  // let developerData = await Promise.all(developers.map(async d =>
+  //             axios.get(`https://api.github.com/users/${d}`)));
+  let developerData = await developerInfo(developers)
+  console.log(developerData)
+  let devDataJson = developerData.map(dev => ({ name: dev.data.name, bio: dev.data.bio }));
+
+  return res.send(JSON.stringify(devDataJson));
 });
 
+async function developerInfo(input){
+  let results = await Promise.all(input.map(async d =>
+                 axios.get(`https://api.github.com/users/${d}`)));
+    // try {
+    //   await axios.get(`https://api.github.com/users/${d}`);
+    // }
+    // catch {
+    //   throw new NotFoundError(`Cannot find user with username ${d}`);
+    // }})
+  return results
+}
 
 
 app.listen(3000);
