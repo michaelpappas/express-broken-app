cce4df8d6fb5a14ea71c5cdf07b72687d0e05d86
michaelpappas <pappas.michael@gmail.com>
2023-01-07 22:08:31 -0800

    json returning correctly

--- app.js
+++ app.js
@@ -5,27 +5,15 @@ var app = express();
 // Parse request bodies for JSON
 app.use(express.json());
 
-app.get('/', async function (req, res, next) {
-  console.log('req.body.developers- ', req.body.developers)
-  // let numbers = [1,2,3,4]
-  // let numberInfo = numbers.map(async num =>
-  //   axios.get(`http://numbersapi.com/${num}`));
-  // console.log("numberInfo-", await Promise.all(numberInfo))
-  // debugger;
+app.post('/', async function (req, res, next) {
+  let results = await Promise.all(req.body.developers.map(async d =>
+              axios.get(`https://api.github.com/users/${d}`)));
 
-  let results = req.body.developers.map(async d => axios.get(`https://api.github.com/users/${d}`))
-  let returnedResults = await Promise.all(results)
-  debugger;
-  // let out = results.map(r => ({ "name": r.data.name, "bio": r.data.bio }));
-  // console.log("out- ", out)
+  let out = results.map(r => ({ "name": r.data.name, "bio": r.data.bio }));
 
+  return res.send(JSON.stringify(out));
 });
 
-// async function getUsersData(userName){
-//   for()
-//   return await axios.get(`https://api.github.com/users/${username}`)
-
-// }
 
 
 app.listen(3000);
