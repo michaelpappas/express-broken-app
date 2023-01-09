6a2e0c7c82d7e1e280694fec10da0195ab7ec772
michaelpappas <pappas.michael@gmail.com>
2023-01-08 16:52:58 -0800

    separated api response array formatting to it's own function

--- app.js
+++ app.js
@@ -7,33 +7,44 @@ const { NotFoundError } = require("./expressError");
 // Parse request bodies for JSON
 app.use(express.json());
 
-/** takes in the usernames in the developers array from the request
+/** Takes in the usernames in the developers array from the request
    * returns json with the name and bio of each developer
    */
 app.post('/', async function (req, res, next) {
-
   const developers = req.body.developers
 
-  // let developerData = await Promise.all(developers.map(async d =>
-  //             axios.get(`https://api.github.com/users/${d}`)));
   let developerData = await developerInfo(developers)
-  console.log(developerData)
-  let devDataJson = developerData.map(dev => ({ name: dev.data.name, bio: dev.data.bio }));
+
+  let devDataJson = parseGithubData(developerData)
 
   return res.send(JSON.stringify(devDataJson));
 });
 
+/** Takes array of usernames and returns array of the response data from github
+ * if a bad username is provided it will throw a NotFoundError
+*/
 async function developerInfo(input){
+
   let results = await Promise.all(input.map(async d =>
-                 axios.get(`https://api.github.com/users/${d}`)));
-    // try {
-    //   await axios.get(`https://api.github.com/users/${d}`);
-    // }
-    // catch {
-    //   throw new NotFoundError(`Cannot find user with username ${d}`);
-    // }})
+    axios.get(`https://api.github.com/users/${d}`)));
+
+  console.log("in developerInfo- ", results)
+  for(let i=0; i<results.length; i++){
+      if (results[i].data.name === null){
+        throw new NotFoundError(`Cannot find user with username ${input[i]}`)
+      }
+    }
+
   return results
 }
+/** Parses name and bio from array of Github API responses
+ * returns array of objects
+ */
+function parseGithubData(data){
+  return data.map(dev => ({ name: dev.data.name, bio: dev.data.bio }));
+}
+
+
 
 
 app.listen(3000);
